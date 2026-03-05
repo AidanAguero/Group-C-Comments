1d4ed7fff564066d0ae74ee8072c961519d2596c
AidanAguero <aaaguero@cpp.edu>
2026-03-04 20:16:20 -0800

    Add hitpoints.h with health and damage variables

---
+++ hitpoints.h
@@ -0,0 +1,16 @@
+#pragma once
+
+//Health pool that will be altered based on enemy/type
+int enemy_health = 10;
+int max_hp = 10;
+
+//Weapon damage that can be multiplied
+int leaf_blade = 5;
+int infernal_wand = 5;
+int aquatic_shield = 5;
+
+//damage
+int damage = 2;
+int damage_taken;
+
+void equipment();
